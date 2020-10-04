bd4702ab817cd96a000a3c9692562f61394c865c
doncardozo <oa.cardozoz@gmail.com>
2020-10-04 00:34:13 -0300

    update

--- client/src/store/store-tasks.js
+++ client/src/store/store-tasks.js
@@ -37,7 +37,7 @@ const actions = {
     })
     .catch(err => console.log(err))
   },
-  updateTask({commit}, payload){      
+  updateTask({commit, dispatch}, payload){      
     axiosInstance
     .put(`/api/task-update/${payload.updates.id}/`, payload.updates)
     .then((resp) => {      
